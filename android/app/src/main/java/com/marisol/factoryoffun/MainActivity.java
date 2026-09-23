33fe26fe2e6ec262c4038d1cdfe7322c55a79c5e
Vighneswaran-official <vighneswaran.personal@gmail.com>
2026-09-23 21:03:47 +0530

    feat: setup capacitor android platform and configure firebase google auth in android/app

---
+++ android/app/src/main/java/com/marisol/factoryoffun/MainActivity.java
@@ -0,0 +1,5 @@
+package com.marisol.factoryoffun;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marisol.factoryoffun',
+  appName: 'Marisol Factory of Fun',
+  webDir: 'dist'
+};
+
+export default config;
